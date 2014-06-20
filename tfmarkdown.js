4b9447185c4ee922f8dc8c7c0df30692168af677
Thomas Baron <thomasjbaron@hotmail.com>
2014-06-20 00:17:25 -0400

    Added support for reference-style links.

--- tfmarkdown.js
+++ tfmarkdown.js
@@ -18,9 +18,6 @@ var tfmarkdown = {
 			return '<blockquote>'+text+'</blockquote>';
 		});
 		md = md.replace(/^(((\t+|\s{2,}).*\n)+)/gm, function(all, text){
-			console.log('---');
-			console.log(text);
-			console.log('---');
 			text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;');
 			text = text.replace(/\n/g, '<br />\n');
 			return '<code>'+text+'</code>\n';
@@ -36,13 +33,26 @@ var tfmarkdown = {
 			return '<ol>'+all+'</ol>';
 		});
 
-		// Paragraphs
-		md = md.replace(/(^|\n{2,})([^<])(.*)/g, '\n<p>$2$3</p>');
-
 		// Bold, italics and links
 		md = md.replace(/(\*\*|__)(.*)(\*\*|__)/g, '<b>$2</b>');
 		md = md.replace(/[\*_](.*)[\*_]/g, '<i>$1</i>');
 		md = md.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+
+		// Reference-style links
+		md = md.replace(/\[(.*?)\]\s*\[(.*?)\]/g, function(all, text, ref) {
+			var path = '';
+			ref = ref.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+			md.replace(new RegExp('\\\['+ref+'\\\]:\\s*(.*)'), function(all2, path2) {
+				path = path2;
+			});
+			return '<a href="'+path+'">'+text+'</a>';
+		});
+
+		// Remove reference lines
+		md = md.replace(/\[.*\]:.*/g, '');
+
+		// Paragraphs
+		md = md.replace(/(^|\n{2,})([^<])(.*)/g, '\n<p>$2$3</p>');
 
 		// Remove extra line breaks etc
 		md = md.replace('<code><br />', '<code>');
